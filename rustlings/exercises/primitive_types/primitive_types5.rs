40a52a65327db774f483d80ee25392db4540b860
Ryan Anderson <r.garrett.anderson@gmail.com>
2019-07-22 14:43:35 -0500

    init commit

---
+++ rustlings/exercises/primitive_types/primitive_types5.rs
@@ -0,0 +1,44 @@
+// primitive_types5.rs
+// Destructure the `cat` tuple so that the println will work.
+// Scroll down for hints!
+
+fn main() {
+    let cat = ("Furry McFurson", 3.5);
+
+    println!("{} is {} years old.", cat.0, cat.1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Take a look at the Data Types -> The Tuple Type section of the book:
+// https://doc.rust-lang.org/book/ch03-02-data-types.html#the-tuple-type
+// Particularly the part about destructuring (second to last example in the section).
+// You'll need to make a pattern to bind `name` and `age` to the appropriate parts
+// of the tuple. You can do it!!
